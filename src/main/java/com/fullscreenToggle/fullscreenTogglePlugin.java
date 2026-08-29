115a0d2b7753bd8215fa2d0d5be9f0002300d92b
windowwasher7 <kingmeepers2@gmail.com>
2026-08-29 17:22:55 -0600

    named everything

--- src/main/java/com/fullscreenToggle/fullscreenTogglePlugin.java
+++ src/main/java/com/fullscreenToggle/fullscreenTogglePlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.fullscreenToggle;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Fullscreen Toggle"
 )
-public class ExamplePlugin extends Plugin
+public class fullscreenTogglePlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private fullscreenToggleConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	fullscreenToggleConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(fullscreenToggleConfig.class);
 	}
 }
--- src/test/java/com/fullscreenToggle/fullscreenToggleTest.java
+++ src/test/java/com/fullscreenToggle/fullscreenToggleTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.fullscreenToggle;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class fullscreenToggleTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(fullscreenTogglePlugin.class);
 		RuneLite.main(args);
 	}
 }
